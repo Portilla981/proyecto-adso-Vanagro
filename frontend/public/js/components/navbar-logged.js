b552bb9704dfbbe281247faca486b2e1ae988812
Portilla81 <jdportilla81@gmail.com>
2025-10-31 16:19:55 -0500

    fix portilla: Mejoramiento de vistas y navbar

--- frontend/public/js/components/navbar-logged.js
+++ frontend/public/js/components/navbar-logged.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const navbarLoggedContainer = document.querySelector(".navbar-logged-in-container");
+    const navbarLoggedContainer = document.querySelector(".navbar-simple");
     
     if (navbarLoggedContainer) {
-        fetch("/frontend/public/views/components/navbar-logged-in.html")
+        fetch("/frontend/public/views/components/navbar-logged.html")
         .then(response => response.text())
         .then(html => {
             navbarLoggedContainer.innerHTML = html;
