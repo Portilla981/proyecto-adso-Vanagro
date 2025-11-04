bcb3383af8f429ee5bfe95c3ab3674ee63fb3645
Portilla81 <jdportilla81@gmail.com>
2025-11-04 18:13:39 -0500

    fix Portilla: Se adecuo las interfaces nuevas y estilos requeridos

--- frontend/public/js/components/producter-profile.js
+++ frontend/public/js/components/producter-profile.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const cuentaContainer = document.querySelector(".producter-profile");
     
     if (cuentaContainer) {
-        fetch("/frontend/public/views/components/form-edit-user.html")
+        fetch("/frontend/public/views/components/producter-profile.html")
             .then(response => response.text())
             .then(data => {
                 cuentaContainer.innerHTML = data;
