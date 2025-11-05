4cdce24de74d33cad4d86d0c9f2708e5d22d02e6
Portilla81 <jdportilla81@gmail.com>
2025-11-05 15:50:57 -0500

    docs Portilla: Adecuaciones de cambios por parte del merge

---
+++ frontend/public/js/components/product_details-preview.js
@@ -0,0 +1,12 @@
+
+  document.addEventListener("DOMContentLoaded", function () {
+  const msjContainer = document.querySelector(".product-details__container");
+  if (msjContainer) {
+    fetch("/frontend/public/views/components/product-preview.html")
+      .then(response => response.text())
+      .then(data => {
+        msjContainer.innerHTML = data;
+      })
+      .catch(error => console.error("Error al cargar los detalles del producto:", error));
+  }
+});
